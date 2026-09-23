b450b69209adb9095620a108784525c751c98564
Anth396 <antlsr396@gmail.com>
2026-09-24 00:32:23 +0800

    Cat can move

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem; // 1. Added the new Input System namespace
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 10;
+    public float maxSpeed = 20;
+    public float upSpeed = 10;
+    private bool onGroundState = true;
+    private bool jumpRequest = false;
+    private SpriteRenderer catSprite;
+    private bool faceRightState = true;
+    private Rigidbody2D catBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Set to be 30 FPS
+        Application.targetFrameRate = 30;
+        catBody = GetComponent<Rigidbody2D>();
+        catSprite = GetComponent<SpriteRenderer>();
+        catBody.constraints = RigidbodyConstraints2D.FreezeRotation; // Fixed repetitive GetComponent call
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check if keyboard is connected
+        if (Keyboard.current == null) return;
+
+        // 2. Fixed legacy Input.GetKeyDown checks
+        if (Keyboard.current.spaceKey.wasPressedThisFrame && onGroundState)
+        {
+            jumpRequest = true;
+        }
+        
+    }
+
+    // FixedUpdate is called 50 times a second
+    void FixedUpdate()
+    {
+        if (Keyboard.current == null) return;
+
+        // 3. Fixed legacy Input.GetAxisRaw("Horizontal")
+        float moveHorizontal = 0f;
+        if (Keyboard.current.dKey.isPressed) moveHorizontal = 1f;
+        else if (Keyboard.current.aKey.isPressed) moveHorizontal = -1f;
+    
+        if (Mathf.Abs(moveHorizontal) > 0)
+        {
+            Vector2 movement = new Vector2(moveHorizontal, 0);
+            
+            // Note: In newer Unity versions, 'linearVelocity' is used instead of 'velocity'
+            if (catBody.linearVelocity.magnitude < maxSpeed)
+                catBody.AddForce(movement * speed);
+        }
+        
+        // 4. Fixed legacy Input.GetKeyUp
+        bool noMovementKeysPressed = Keyboard.current.aKey.wasReleasedThisFrame || Keyboard.current.dKey.wasReleasedThisFrame;
+        if (noMovementKeysPressed)
+        {
+            // stop
+            catBody.linearVelocity = Vector2.zero;
+        }
+
+        if (jumpRequest)
+        {
+            catBody.AddForce(Vector2.up * upSpeed, ForceMode2D.Impulse);
+            onGroundState = false;
+            jumpRequest = false;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Ground")) onGroundState = true;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("Collided with goomba!");
+        }
+    }
+}
